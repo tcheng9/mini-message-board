4a954a5c9c0c40accbe4513f7dce2efe0e820eb3
tcheng9 <tcheng789@gmail.com>
2022-09-07 23:59:36 -0500

    update

--- mini-message-board/routes/index.js
+++ mini-message-board/routes/index.js
@@ -1,6 +1,3 @@
-var express = require('express');
-var router = express.Router();
-
 const messages = [
   {
     text: "Hi there!",
@@ -14,10 +11,32 @@ const messages = [
   }
 ];
 
+var express = require('express');
+var router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  // res.render('index', { title: "Mini Messageboard", messages: messages })
+  res.render('index', { title: 'Mini Messageboard', messages: messages, });
 });
 
+
+// Form Get
+
+router.get('/new', function (req, res, next) {
+  res.render('form');
+});
+
+//Form Post
+router.post('/new', function(req,res,next){
+  let messageText = req.body.messageText;
+  let messageUser = req.body.messageUser;
+  messages.push({text: messageText, user: messageUser, added: new Date()});
+ 
+  res.redirect('/')
+})
 module.exports = router;
+
